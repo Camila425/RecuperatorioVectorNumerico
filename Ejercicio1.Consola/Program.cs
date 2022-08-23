738518d5759996edda2a68e79792c4a1e3c55101
camila <camila@LAPTOP-N0GHACTF>
2022-08-22 23:34:26 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1.Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using EjercicioVectorNumerico.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VectorNumerico vectorNumerico = new VectorNumerico();
+
+            if (vectorNumerico.Validar())
+            {
+               vectorNumerico.MostrarContenido();
+                Console.WriteLine(vectorNumerico.ToString());
+                Console.WriteLine(vectorNumerico.MostrarEnLinea());
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjercicioVectorNumerico.Entidades/VectorNumerico.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioVectorNumerico.Entidades
+{
+    public class VectorNumerico
+    {
+
+        private int[] Numeros;
+        public int Dimension { get; set; }
+        public VectorNumerico(int dimension)
+        {
+            Dimension = dimension;
+        }
+
+        public VectorNumerico():this(10)
+        {
+
+        }
+        public bool Validar()
+        {
+            return Dimension > 1;
+        }
+
+        private void  NumeroAzar()
+        {
+            int NumeroAzar=0;
+            Random random = new Random();
+            for (int i = 1; i < 50; i++)
+            {
+                NumeroAzar = random.Next(1, 50);
+            }
+            Console.WriteLine($"{NumeroAzar}");
+        }
+
+        public void MostrarContenido()
+        {
+            Numeros = new int[Dimension];
+            var numeroA = 1;
+            var numeroB = 2;
+            Numeros[0] = numeroA;
+            Numeros[1] = numeroB;
+            for (int contador = 2; contador < Dimension; contador++)
+            {
+                Numeros[contador] = Numeros[contador - 2] + Numeros[contador - 1];
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder linea = new StringBuilder();
+            foreach (var numero in Numeros)
+            {
+                linea.AppendLine($"{numero}");
+            }
+            return linea.ToString();
+        }
+
+        public string MostrarEnLinea()
+        {
+            StringBuilder vector = new StringBuilder();
+            foreach (var numero in Numeros)
+            {
+                vector.Append($"{numero}");
+            }
+            return vector.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null  || !(obj is VectorNumerico))
+            {
+                return false;
+            }
+            return this.Dimension == ((VectorNumerico)obj).Dimension;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(VectorNumerico dimension1,VectorNumerico dimension2)
+        {
+            return dimension1.Equals(dimension2);
+        }
+
+        public static bool operator !=(VectorNumerico dimension1, VectorNumerico dimension2)
+        {
+            return !(dimension1 == dimension2);
+        }
+
+        public static VectorNumerico operator +(VectorNumerico dimension, VectorNumerico dimension2)
+        {
+            return dimension + dimension2;
+        }
+
+        public static VectorNumerico operator -(VectorNumerico dimension, VectorNumerico dimension2)
+        {
+            return dimension - dimension2;
+        }
+
+        public static VectorNumerico operator *(VectorNumerico vectorNumerico, int numero)
+        {
+            return vectorNumerico * numero;
+        }
+
+    
+
+    }
+}
